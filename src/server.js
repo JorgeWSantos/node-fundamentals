356015fea0b67f81e44041479b29aba62ffeec9f
JorgeWS <jorgews.dev@gmail.com>
2024-02-17 17:16:43 -0300

    feat: 🎸 Create local database

--- src/server.js
+++ src/server.js
@@ -1,21 +1,21 @@
 import http from 'node:http';
+import { Database } from './database.js';
 import { json } from './middlewares/json.js';
 
-
-const users = [];
-
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
 
     await json(req, res)
 
-    if (method === 'GET' && url === '/users')
-        return res
-            .end(JSON.stringify(users))
+    if (method === 'GET' && url === '/users') {
+        const users = database.select('users')
+        return res.end(JSON.stringify(users))
+    }
 
     if (method === 'POST' && url === '/users') {
-        users.push(req.body)
+        database.insert('users', req.body)
         return res.writeHead(201).end()
     }
 
